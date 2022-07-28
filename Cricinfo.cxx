2f4a5efe1c981729bf05fd11704422cfca1e1cf2
Deepak Mehta <2970288+deepakmehta@users.noreply.github.com>
2022-07-28 19:00:28 +0530

    Initial Checkin for LLD problems

---
+++ Cricinfo.cxx
@@ -0,0 +1,17 @@
+/*
+Cricinfo is a sports news website exclusively for the game of cricket.
+The site features live coverage of cricket matches containing ball-by-ball commentary
+and a database for all the historic matches. The site also provides news and articles about cricket.
+
+System Requirements#
+* We will focus on the following set of requirements while designing Cricinfo:
+* The system should keep track of all cricket-playing teams and their matches.
+* The system should show live ball-by-ball commentary of cricket matches.
+* All international cricket rules should be followed.
+* Any team playing a tournament will announce a squad (a set of players) for the tournament.
+* For each match, both teams will announce their playing-eleven from the tournament squad.
+* The system should be able to record stats about players, matches, and tournaments.
+* The system should be able to answer global stats queries like, “Who is the highest wicket taker of all time?”, “Who has scored maximum numbers of 100s in test matches?”, etc.
+* The system should keep track of all ODI, Test and T20 matches.
+*/
+
